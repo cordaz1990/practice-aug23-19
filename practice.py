39ae474bdb3017934147eed792414d837b4d8aeb
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 18:32:45 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,20 @@
-print('hello')
-pri
+def find_center(rect):
+    p = Point()
+    p.x = rect.corner.x + rect.width/2
+    p.y = rect.corner.y + rect.height/2
+    return p
+  
+  
+>>> center = find_center(box)
+>>> print_point(centers)
+(50, 100)
+
+box.width = box.width + 50
+box.height = box.height + 100
+
+def grow_retangle(rect, dwidth, dheight):
+    rect.width += dwidth
+    rect.height += dheight
+    
+>>> box.width, box.height
+(150.0, 300.0)
